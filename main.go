a37eabf149cb496379f74aa8d963261724608656
cvando <clement.vandoolaeghe@gmail.com>
2018-03-22 17:52:38 +0100

    Add 404

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 import (
   "net/http"
+  "regexp"
 )
 
 func redirect(w http.ResponseWriter, req *http.Request) {
-    // remove/add not default ports from req.Host
+    matched, _ := regexp.MatchString("[0-9]+.[0-9]+.[0-9]+.[0-9]+", req.Host)
+    if matched == true {
+      w.WriteHeader(404)
+      return
+    }
     target := "https://" + req.Host + req.URL.Path
     if len(req.URL.RawQuery) > 0 {
         target += "?" + req.URL.RawQuery
